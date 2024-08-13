8a087dc0760d3623151bf7242e1209db10190d32
Ashish <ashishmalviya886@gmail.com>
2024-08-13 19:12:29 +0530

    Add AuthInterceptor for token-based authorization and configure AppModule
    - **AuthInterceptor:**
      - Implemented `AuthInterceptor` to automatically attach authorization tokens to HTTP requests.
      - The interceptor retrieves the token from local storage and includes it in the `Authorization` header if available.
    
    - **AppModule:**
      - Configured `AppModule` to include `AuthInterceptor` in the HTTP interceptors.
      - Added `MatSnackBarModule` for Angular Material Snackbar functionality.
      - Imported necessary modules including `BrowserModule`, `AppRoutingModule`, `BrowserAnimationsModule`, and `SharedModule`.
    
    These changes integrate token-based authorization for secure API interactions and configure the application module to support Angular Material components and request interception.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './interceptor/auth-interceptor.service';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule,
+    MatSnackBarModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
