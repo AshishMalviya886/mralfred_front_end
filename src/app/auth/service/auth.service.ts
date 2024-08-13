cd5a12fe32ecad73bc73b81c63b2d754447cd935
Ashish <ashishmalviya886@gmail.com>
2024-08-13 19:03:38 +0530

    Implement AuthService with login and logout methods
    Updated `AuthService` to handle authentication-related API interactions:
    
    - **Login Method:**
      - Sends a POST request to the `auth/login` endpoint with email and password.
    
    - **Logout Method:**
      - Sends a POST request to the `auth/logout` endpoint with an Authorization header containing the user's token.
    
    **Notes:**
    - Imported `environment` from the correct path (`src/environments/environment.prod`).
    - Added `HttpHeaders` to include the token in the logout request.
    
    These updates enable the service to interact with authentication endpoints for user login and logout functionality.

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environment/environment.prod';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private api_url = environment.apiURL;
+
+  constructor(private http: HttpClient) { }
+
+  login(data:{email:string,password:string}){
+    return this.http.post<any>(this.api_url + "auth/login",data);
+  }
+
+  logout(){
+    const token = localStorage.getItem('userToken');
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+    return this.http.post<any>(this.api_url + "auth/logout", { headers });
+  }
 }
