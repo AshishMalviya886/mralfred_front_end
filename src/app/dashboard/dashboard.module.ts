28020dc0cdf00feb51b712528e61dd8009635f05
Ashish <ashishmalviya886@gmail.com>
2024-08-13 17:49:54 +0530

    Generate home and add-post components in dashboard/pages
    Created the following components under the `dashboard/pages` directory:
    
    - `home`: A component for displaying the main dashboard view.
    - `add-post`: A component for adding new posts or content to the dashboard.
    These components will be used to build out the dashboard functionality and provide user interfaces for managing content.

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { AddPostComponent } from './pages/add-post/add-post.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    AddPostComponent
+  ],
   imports: [
     CommonModule,
     DashboardRoutingModule
