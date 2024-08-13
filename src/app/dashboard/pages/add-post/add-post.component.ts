b311c4f7f078c12ab9ab9933f0300ad8833fbf3b
Ashish <ashishmalviya886@gmail.com>
2024-08-13 20:17:44 +0530

    Enhance AddPostComponent for Create and Update Operations
    - Refactored the `AddPostComponent` to handle both creation and updating of posts:
      - Added logic to determine if the component is in "Add" or "Update" mode based on the presence of `postId`.
      - Implemented the `patchForm` method to pre-fill the form with existing post data when editing.
      - Updated the `addPost` method to dispatch the appropriate action (Create or Update) based on whether `postId` is provided.
      - Enhanced form validation and handling for both creating and updating posts.
      - Added `ngOnInit` method placeholder for potential future use.
    
    These changes ensure that the component can dynamically switch between creating a new post and updating an existing one, providing a seamless user experience.

--- src/app/dashboard/pages/add-post/add-post.component.ts
+++ src/app/dashboard/pages/add-post/add-post.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { select, Store } from '@ngrx/store';
+import { CreatePost, GetPostById, UpdatePostById } from 'src/app/store/actions/user.action';
+import { getPostById } from 'src/app/store/selectors/user.selectors';
 
 @Component({
   selector: 'app-add-post',
@@ -7,4 +12,53 @@ import { Component } from '@angular/core';
 })
 export class AddPostComponent {
 
+  addPostForm: FormGroup;
+  isSubmitted: boolean = false;
+  postId:string = '';
+  title:string = 'Add Post';
+
+  constructor( private dialogRef: MatDialogRef<AddPostComponent>,private fb:FormBuilder,private store:Store,@Inject(MAT_DIALOG_DATA) public dialogData: any,){
+    this.addPostForm = this.fb.group({
+      title: ['',[Validators.required]],
+      description: ['',[Validators.required]]
+    })
+    if (this.dialogData) {
+      this.title = 'Update Post'
+      this.postId = this.dialogData.postId;
+      this.patchForm();
+    }
+  }
+
+  ngOninit(){
+  }
+
+  addPost(){
+    this.isSubmitted = true;
+    if(this.addPostForm.valid){
+      const { title, description } = this.addPostForm.value;
+      if(this.postId !== ''){
+        this.store.dispatch(UpdatePostById({id:this.postId,title:title,description:description}));
+      } else {
+        this.store.dispatch(CreatePost({title:title,description:description}));
+      }
+      this.addPostForm.reset();
+      this.dialogRef.close("success");
+    }
+  }
+
+  patchForm(){
+    this.store.dispatch(GetPostById({id:this.postId}));
+    this.store.select(getPostById).subscribe((data)=>{
+      if(data){
+        this.addPostForm.patchValue({
+          title: data.title,
+          description: data.description
+        })
+      }
+    })
+  }
+
+  cancel(){
+    this.dialogRef.close("success");
+  }
 }
