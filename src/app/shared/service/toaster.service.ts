bbfe0a57b845a4f98066cf6653ea418a177124c8
Ashish <ashishmalviya886@gmail.com>
2024-08-13 19:37:26 +0530

    Add ToastService for displaying success and error notifications
    - **ToastService:**
      - Added a new service for displaying notifications using Angular Material's `MatSnackBar`.
      - Implemented two methods:
        - `showSuccess(message: string)`: Displays a success notification with a `success-snackbar` style.
        - `showError(message: string)`: Displays an error notification with an `error-snackbar` style.
      - Configured notifications to be visible for 30 seconds with a "Close" button.
    
    - **Styling for Notifications:**
      - Notifications are styled with `success-snackbar` and `error-snackbar` classes for differentiation.
    
    This service provides a centralized way to show user feedback notifications across the application, enhancing the user experience by providing timely and clear messages for both success and error scenarios.

---
+++ src/app/shared/service/toaster.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastService {
+
+  constructor(private snackBar: MatSnackBar) { }
+
+  showSuccess(message: string): void {
+    this.snackBar.open(message, 'Close', {
+      duration: 30000,
+      panelClass: ['success-snackbar']
+    });
+  }
+
+  showError(message: string): void {
+    this.snackBar.open(message, 'Close', {
+      duration: 30000,
+      panelClass: ['error-snackbar']
+    });
+  }
+}
