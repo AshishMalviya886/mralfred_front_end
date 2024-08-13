fcbace5fc5e84e9f423e869a26657dac44a0c2b8
Ashish <ashishmalviya886@gmail.com>
2024-08-13 18:24:04 +0530

    Update SharedModule with common imports and exports
    Updated `shared.module.ts` to include commonly used modules and components:
    
    - Imported `CommonModule`, `FormsModule`, `ReactiveFormsModule`, `MatDialogModule`, `HttpClientModule`, `MatTableModule`, and `MatIconModule`.
    - Exported `FormsModule`, `ReactiveFormsModule`, `MatDialogModule`, `HttpClientModule`, `MatTableModule`, and `MatIconModule` for use in other modules.
    
    This configuration centralizes commonly used modules and Angular Material components, promoting reusability and consistency across the application.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    HttpClientModule,
+    MatTableModule,
+    MatIconModule,
+  ],
+  providers: [],
+  exports: [FormsModule,ReactiveFormsModule,MatDialogModule,HttpClientModule,MatTableModule,MatIconModule]
 })
 export class SharedModule { }
